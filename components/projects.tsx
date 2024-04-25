4d9d9598373e1198e71da13cecafb1e4d0150ab2
Joao Victor Pedrosa <joaovictor.controlit@gmail.com>
2024-04-25 11:44:27 -0300

    theme-context.tsx and experience.tsx

--- components/projects.tsx
+++ components/projects.tsx
@@ -4,10 +4,13 @@ import React from "react";
 import SectionHeading from "./section-heading";
 import { projectsData } from "@/lib/data";
 import Project from "./project";
+import { useSectionInView } from "@/lib/hooks";
 
 export default function Projects() {
+  const { ref } = useSectionInView("Projetos");
+
   return (
-    <section id="projects" className="scroll-mt-28 mb-28">
+    <section id="projects" className="scroll-mt-28 mb-28" ref={ref}>
       <SectionHeading>Meus projetos</SectionHeading>
       <div>
         {projectsData.map((project, index) => (
